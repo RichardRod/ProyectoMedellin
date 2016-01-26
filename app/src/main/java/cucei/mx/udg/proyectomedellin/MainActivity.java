360352f8c6babbc2a28a2f5bef228121851a08ce
RichardRod <ricardoharo@outlook.com>
2016-01-25 18:09:23 -0600

    Primer Commit: Inicio de la aplicacion.

--- app/src/main/java/cucei/mx/udg/proyectomedellin/MainActivity.java
+++ app/src/main/java/cucei/mx/udg/proyectomedellin/MainActivity.java
@@ -3,6 +3,10 @@ package cucei.mx.udg.proyectomedellin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+ * @autor: Los dos barbaros
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
